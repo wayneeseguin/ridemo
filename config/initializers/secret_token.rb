1cb303a1ddfaac3af1d8be596c6b6feb2ca3df84
Wayne E. Seguin <wayneeseguin@gmail.com>
2011-04-12 15:43:48 -0400

    Initial generated rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = 'd94dff99fc14716ae6b625cb1926de5c981e17dabb0fa57699a37a2915e43da8b24f1c632c1e2a6653dfe81d6b05b6d61c5fae7c448ff7c651c03eed2d621629'
